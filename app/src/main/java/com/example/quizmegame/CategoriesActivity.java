c88b4df4a46acf4f543d3a896c190b86bdccac9d
Filippos Euthymiadis <filippefthy@outlook.com.gr>
2022-05-29 18:12:45 +0300

    Commit categories

--- app/src/main/java/com/example/quizmegame/CategoriesActivity.java
+++ app/src/main/java/com/example/quizmegame/CategoriesActivity.java
@@ -15,17 +15,71 @@ public class CategoriesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
-        final CardView geography = findViewById(R.id.geo_categ);
+        final CardView geo_categ = findViewById(R.id.geo_categ);
         final CardView entert_categ = findViewById(R.id.entert_categ);
         final CardView sports_categ = findViewById(R.id.sports_categ);
         final CardView history_categ = findViewById(R.id.history_categ);
         final CardView science_categ = findViewById(R.id.science_categ);
         final CardView art_categ = findViewById(R.id.art_categ);
 
-        geography.setOnClickListener(new View.OnClickListener() {
+
+        geo_categ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 selectedCategoryName = "geography";
+
+
+            }
+        });
+
+        entert_categ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                selectedCategoryName = "entertainment";
+
+
+            }
+        });
+
+        sports_categ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                selectedCategoryName = "sports";
+
+
+            }
+        });
+
+        history_categ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                selectedCategoryName = "history";
+
+
+            }
+        });
+
+        science_categ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                selectedCategoryName = "science";
+
+
+            }
+        });
+
+        art_categ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                selectedCategoryName = "art";
+
+
             }
         });
 
