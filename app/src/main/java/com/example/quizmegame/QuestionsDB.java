78182f54247990569f15ffee0745370a7ba4cb9d
Filippos Euthymiadis <filippefthy@outlook.com.gr>
2022-06-01 18:18:23 +0300

    Initial QuestionsDB

---
+++ app/src/main/java/com/example/quizmegame/QuestionsDB.java
@@ -0,0 +1,77 @@
+package com.example.quizmegame;
+
+import java.util.ArrayList;
+
+public class QuestionsDB {
+
+    private static ArrayList<Question> geographyQuestions(){
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+        return questionList;
+    }
+
+    private static ArrayList<Question> entertainmentQuestions(){
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+
+        return questionList;
+    }
+
+    private static ArrayList<Question> sportsQuestions(){
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+        return questionList;
+    }
+
+    private static ArrayList<Question> historyQuestions(){
+
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+
+        return questionList;
+    }
+
+    private static ArrayList<Question> scienceQuestions(){
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+
+        return questionList;
+    }
+
+    private static ArrayList<Question> artQuestions(){
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+        return questionList;
+    }
+
+    private static ArrayList<Question> anyCategory(){
+
+
+        final ArrayList<Question> questionList = new ArrayList<>();
+
+
+
+        return questionList;
+    }
+
+
+
+}
