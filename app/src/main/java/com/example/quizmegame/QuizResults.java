bb8d4c14fae1b1ee6814add74601ab1dfc6d612d
Eleftheria Lito Michanetzi <ellimich15@gmail.com>
2022-06-12 21:06:38 +0300

    Final Quiz Results

--- app/src/main/java/com/example/quizmegame/QuizResults.java
+++ app/src/main/java/com/example/quizmegame/QuizResults.java
@@ -8,21 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class QuizResults extends AppCompatActivity {
 
-    private Button playAgain_btn;
-    private Button exit_btn;
     private TextView correctAnswers;
     private TextView coins_earned;
     private TextView exp_earned;
+    FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
 
-        playAgain_btn = findViewById(R.id.playAgain_btn);
-        exit_btn = findViewById(R.id.exit_btn);
+        Button playAgain_btn = findViewById(R.id.playAgain_btn);
+        Button exit_btn = findViewById(R.id.exit_btn);
         correctAnswers = findViewById(R.id.correct_answ_num);
         coins_earned = findViewById(R.id.coins_gained);
         exp_earned = findViewById(R.id.exp_gained);
@@ -50,15 +53,33 @@ public class QuizResults extends AppCompatActivity {
     }
 
     private void calcResults(int correctAnswersNum){
+        int coins = correctAnswersNum * 50;
+        int tickets = correctAnswersNum * 30;
+        int points = correctAnswersNum * 80;
+        int lives = -1;
 
-        if(correctAnswersNum == 0){
-            correctAnswers.setText((0 + "/" + "10"));
-        }
-        else{
-            correctAnswers.setText((correctAnswersNum + "/" + "10"));
-            //coins_earned.setText(correctAnswersNum);
-            //coins_earned.setText(correctAnswersNum);
-        }
+        database = FirebaseFirestore.getInstance();
 
+        database.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .update("coins", FieldValue.increment(coins));
+
+        database.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .update("tickets", FieldValue.increment(tickets));
+
+        database.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .update("experience", FieldValue.increment(points));
+
+        database.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .update("lives", FieldValue.increment(lives));
+
+
+
+        correctAnswers.setText((correctAnswersNum + "/" + "10"));
+        coins_earned.setText(String.valueOf(coins));
+        exp_earned.setText(String.valueOf(points));
     }
 }
